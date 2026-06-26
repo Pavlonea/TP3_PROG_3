d45f508578382884643e25edfd2559c9f11b5653
Pablo Exequiel Ferreyra <pablo_ferreyra9@outlook.com>
2026-06-26 19:54:41 -0300

    Se agrego el formulario de personas con react

--- js/react-app.js
+++ js/react-app.js
@@ -1,6 +1,70 @@
 function App() {
+
+    const [persona, setPersona] = React.useState({
+        nombre: "",
+        apellido: "",
+        edad: "",
+        altura: "",
+        peso: ""
+    });
+
+    function cambiar(e) {
+        setPersona({
+            ...persona,
+            [e.target.name]: e.target.value
+        });
+    }
+
     return (
-        <h2>Aplicación en React</h2>
+        <>
+            <form>
+
+                <input
+                    name="nombre"
+                    placeholder="Nombre"
+                    value={persona.nombre}
+                    onChange={cambiar}
+                />
+
+                <input
+                    name="apellido"
+                    placeholder="Apellido"
+                    value={persona.apellido}
+                    onChange={cambiar}
+                />
+
+                <input
+                    type="number"
+                    name="edad"
+                    placeholder="Edad"
+                    value={persona.edad}
+                    onChange={cambiar}
+                />
+
+                <input
+                    type="number"
+                    step="0.01"
+                    name="altura"
+                    placeholder="Altura"
+                    value={persona.altura}
+                    onChange={cambiar}
+                />
+
+                <input
+                    type="number"
+                    step="0.1"
+                    name="peso"
+                    placeholder="Peso"
+                    value={persona.peso}
+                    onChange={cambiar}
+                />
+
+                <button>
+                    Agregar
+                </button>
+
+            </form>
+        </>
     );
 }
 
